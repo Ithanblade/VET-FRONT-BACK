4e7a4e60fada3a8a5632325210268f212301750a
Ithan <ithan.camacho@epn.edu.ec>
2025-01-09 16:06:16 -0500

    Proteccion de rutas

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -15,6 +15,7 @@ import Perfil from './paginas/Perfil'
 import { Confirmar } from './paginas/Confirmar'
 import Restablecer from './paginas/Restablecer'
 import { AuthProvider } from './context/AuthProvider'
+import { PrivateRoute } from './routes/PrivateRoute'
 
 
 
@@ -36,18 +37,23 @@ function App() {
           <Route path='recuperar-password/:token' element={<Restablecer />} />
         </Route>
 
-        <Route path='/dashboard' element={<Dashboard/>}>
-          <Route index element={<Perfil/>}/>
-          <Route path='listar' element={<Listar/>}/>
-          <Route path='visualizar/:id' element={<Visualizar/>}/>
-          <Route path='crear' element={<Crear/>}/>
-          <Route path='actualizar/:id' element={<Actualizar/>}/>
-        </Route>
-
-
+        <Route path='dashboard/*' element={
+      <PrivateRoute>
+        <Routes>
+          <Route element={<Dashboard />}>
+            <Route index element={<Perfil />} />
+            <Route path='listar' element={<Listar />} />
+            <Route path='visualizar/:id' element={<Visualizar />} />
+            <Route path='crear' element={<Crear />} />
+            <Route path='actualizar/:id' element={<Actualizar />} />
+          </Route>
+        </Routes>
+      </PrivateRoute>
+      } />
 
 
       </Routes>
+
       </AuthProvider>
     </BrowserRouter>
     </>
